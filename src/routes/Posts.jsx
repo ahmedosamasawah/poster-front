5fa92b383ebda55da8dcec472e85a64822399cae
Ahmed-Sawah <122928535+Ahmed-Sawah@users.noreply.github.com>
2023-07-19 16:38:08 +0300

    upload frontend folder

---
+++ src/routes/Posts.jsx
@@ -0,0 +1,22 @@
+import { Outlet } from "react-router-dom";
+
+import PostsList from "../components/PostsList";
+
+function Posts() {
+  return (
+    <>
+      <Outlet />
+      <main>
+        <PostsList />
+      </main>
+    </>
+  );
+}
+
+export default Posts;
+
+export async function loader() {
+  const response = await fetch("https://poster-ri4g.onrender.com/posts");
+  const resData = await response.json();
+  return resData.posts;
+}
